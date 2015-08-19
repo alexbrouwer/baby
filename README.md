392f95f3bd43a00be05ee7fa3106105c93b3af9d
Alex <brouwer.alexander@gmail.com>
2015-08-19 20:13:38 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+baby
+====
+
+A Symfony project created on August 19, 2015, 6:31 pm.
---
+++ src/Baby/AppBundle/BabyAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Baby\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BabyAppBundle extends Bundle
+{
+}
